e1ed94900b62cdb8b405e2461f6430cdb147129c
mphele <mphelemoswane3@gmail.com>
2026-02-24 15:32:28 +0200

    customer endpoints complete and spaza handlers created

--- hokela-api/cmd/api/main.go
+++ hokela-api/cmd/api/main.go
@@ -5,7 +5,8 @@ import (
 	"log"
 	"os"
 
-	"github.com/gin-gonic/gin"
+	"github.com/glebarez/sqlite"
+	"gorm.io/gorm"
 	// "github.com/joho/godotenv"
 )
 
@@ -14,18 +15,21 @@ func main() {
 	// 	log.Fatal("Failed to load .env")
 	// }
 
+	db, err := gorm.Open(sqlite.Open("test.db"), &gorm.Config{})
+	if err != nil {
+		log.Fatal("failed to connect database: ", err)
+	}
+
+	log.Println("Database connection successfully established")
+
+	db.AutoMigrate(&handlers.Customer{})
+
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = "8080"
 	}
 
-	r := gin.Default()
-
-	api := r.Group("/api/v1")
-	{
-		api.GET("/ping", handlers.HealthCheck)
-
-	}
+	r := handlers.SetupRouter(db)
 
 	log.Printf("Starting :%s", port)
 	r.Run(":" + port)
--- hokela-api/internal/handlers/api.go
+++ hokela-api/internal/handlers/api.go
@@ -5,6 +5,7 @@ import (
 	"time"
 
 	"github.com/gin-gonic/gin"
+	"gorm.io/gorm"
 )
 
 func HealthCheck(c *gin.Context) {
@@ -12,4 +13,24 @@ func HealthCheck(c *gin.Context) {
 		"status":    "Healthy",
 		"timestamp": time.Now().Unix(),
 	})
+}
+
+func SetupRouter(db *gorm.DB) *gin.Engine {
+
+	router := gin.Default()
+
+	customerHandler := &CustomerHandler{DB: db}
+
+	api := router.Group("/api/v1")
+	{
+		api.GET("/ping", HealthCheck)
+		api.GET("/customers/:id", customerHandler.GetCustomer)
+		api.POST("/customers", customerHandler.CreateCustomer)
+		api.PUT("/customers/:id", customerHandler.UpdateCustomer)
+		api.DELETE("/customers/:id", customerHandler.DeleteCustomer)
+
+	}
+
+	return router
+
 }
